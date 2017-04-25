7012215563a794cbdf3cfbc8e66c0581ae48dc3a
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-04-25 15:50:39 +0300

    Handle enter key on tenant switch.

--- aspnet-core/src/AbpCompanyName.AbpProjectName.Web.Mvc/wwwroot/view-resources/Views/Shared/Components/TenantChange/_ChangeModal.js
+++ aspnet-core/src/AbpCompanyName.AbpProjectName.Web.Mvc/wwwroot/view-resources/Views/Shared/Components/TenantChange/_ChangeModal.js
@@ -3,8 +3,7 @@
 
     var _$form = $('form[name=TenantChangeForm]');
 
-    _$form.closest('div.modal-content').find(".save-button").click(function (e) {
-        e.preventDefault();
+    function switchToSelectedTenant () {
 
         var tenancyName = _$form.find('input[name=TenancyName]').val();
 
@@ -18,22 +17,36 @@
             tenancyName: tenancyName
         }).done(function (result) {
             switch (result.state) {
-                case 1: //Available
-                    abp.multiTenancy.setTenantIdCookie(result.tenantId);
-                    //_modalManager.close();
-                    location.reload();
-                    return;
-                case 2: //InActive
-                    abp.message.warn(abp.utils.formatString(abp.localization
-                        .localize("TenantIsNotActive", "AbpProjectName"),
-                        this.tenancyName));
-                    break;
-                case 3: //NotFound
-                    abp.message.warn(abp.utils.formatString(abp.localization
-                        .localize("ThereIsNoTenantDefinedWithName{0}", "AbpProjectName"),
-                        this.tenancyName));
-                    break;
+            case 1: //Available
+                abp.multiTenancy.setTenantIdCookie(result.tenantId);
+                //_modalManager.close();
+                location.reload();
+                return;
+            case 2: //InActive
+                abp.message.warn(abp.utils.formatString(abp.localization
+                    .localize("TenantIsNotActive", "AbpProjectName"),
+                    this.tenancyName));
+                break;
+            case 3: //NotFound
+                abp.message.warn(abp.utils.formatString(abp.localization
+                    .localize("ThereIsNoTenantDefinedWithName{0}", "AbpProjectName"),
+                    this.tenancyName));
+                break;
             }
         });
+    }
+
+    //Handle save button click
+    _$form.closest('div.modal-content').find(".save-button").click(function(e) {
+        e.preventDefault();
+        switchToSelectedTenant();
+    });
+
+    //Handle enter key
+    _$form.find('input').on('keypress', function (e) {
+        if (e.which === 13) {
+            e.preventDefault();
+            switchToSelectedTenant();
+        }
     });
 })(jQuery);
