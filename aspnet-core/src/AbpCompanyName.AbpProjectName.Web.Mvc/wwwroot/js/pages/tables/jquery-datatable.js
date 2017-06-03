907a1ef7ecd74f1ca47e826af05cd6f8ea597afe
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-06-03 05:32:15 +0300

    Added Admin BSB template files, made it initially working.

---
+++ aspnet-core/src/AbpCompanyName.AbpProjectName.Web.Mvc/wwwroot/js/pages/tables/jquery-datatable.js
@@ -0,0 +1,14 @@
+$(function () {
+    $('.js-basic-example').DataTable({
+        responsive: true
+    });
+
+    //Exportable table
+    $('.js-exportable').DataTable({
+        dom: 'Bfrtip',
+        responsive: true,
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
+    });
+});
