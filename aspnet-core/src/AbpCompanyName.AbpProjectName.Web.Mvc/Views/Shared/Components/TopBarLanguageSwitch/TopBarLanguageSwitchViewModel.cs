94e7d110ab06da44a268e9c9e0e6aca71c96fc65
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-06-04 14:37:32 +0300

    Implemented language switch area.

---
+++ aspnet-core/src/AbpCompanyName.AbpProjectName.Web.Mvc/Views/Shared/Components/TopBarLanguageSwitch/TopBarLanguageSwitchViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Abp.Localization;
+
+namespace AbpCompanyName.AbpProjectName.Web.Views.Shared.Components.TopBarLanguageSwitch
+{
+    public class TopBarLanguageSwitchViewModel
+    {
+        public LanguageInfo CurrentLanguage { get; set; }
+
+        public IReadOnlyList<LanguageInfo> Languages { get; set; }
+    }
+}
