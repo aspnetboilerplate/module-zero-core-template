8a132cab1d74de5fdf2ba866ef40338cec0d1c85
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-06-05 07:53:22 +0300

    Add fonts to local and remove Google Analytics.

--- aspnet-core/src/AbpCompanyName.AbpProjectName.Web.Mvc/wwwroot/js/demo.js
+++ aspnet-core/src/AbpCompanyName.AbpProjectName.Web.Mvc/wwwroot/js/demo.js
@@ -72,32 +72,3 @@ function activateNotificationAndTasksScroll() {
         railBorderRadius: '0'
     });
 }
-
-//Google Analiytics ======================================================================================
-addLoadEvent(loadTracking);
-var trackingId = 'UA-30038099-6';
-
-function addLoadEvent(func) {
-    var oldonload = window.onload;
-    if (typeof window.onload != 'function') {
-        window.onload = func;
-    } else {
-        window.onload = function () {
-            oldonload();
-            func();
-        }
-    }
-}
-
-function loadTracking() {
-    (function (i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
-            (i[r].q = i[r].q || []).push(arguments)
-        }, i[r].l = 1 * new Date(); a = s.createElement(o),
-        m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-
-    ga('create', trackingId, 'auto');
-    ga('send', 'pageview');
-}
-//========================================================================================================
