da8455a39aff9523f35f80f57ac4716e8a6d15b3
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-06-08 08:31:21 +0300

    Upgraded ABP and other packages to their latest version.

--- aspnet-core/src/AbpCompanyName.AbpProjectName.Core/Authorization/PermissionChecker.cs
+++ aspnet-core/src/AbpCompanyName.AbpProjectName.Core/Authorization/PermissionChecker.cs
@@ -1,11 +1,10 @@
 ﻿using Abp.Authorization;
 using AbpCompanyName.AbpProjectName.Authorization.Roles;
 using AbpCompanyName.AbpProjectName.Authorization.Users;
-using AbpCompanyName.AbpProjectName.MultiTenancy;
 
 namespace AbpCompanyName.AbpProjectName.Authorization
 {
-    public class PermissionChecker : PermissionChecker<Tenant, Role, User>
+    public class PermissionChecker : PermissionChecker<Role, User>
     {
         public PermissionChecker(UserManager userManager)
             : base(userManager)
