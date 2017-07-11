d98b80c6d42e94d2fbb0a77a58fbecc85ea03ca7
İsmail ÇAGDAŞ <ismailcagdas@gmail.com>
2017-07-11 11:48:26 +0300

    resolved #79: MVC template is aligned with angular UI

--- aspnet-core/src/AbpCompanyName.AbpProjectName.Web.Mvc/Startup/PageNames.cs
+++ aspnet-core/src/AbpCompanyName.AbpProjectName.Web.Mvc/Startup/PageNames.cs
@@ -6,5 +6,6 @@
         public const string About = "About";
         public const string Tenants = "Tenants";
         public const string Users = "Users";
+        public const string Roles = "Roles"; 
     }
 }
