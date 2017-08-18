c85437b8b8469464ffad9df47fd3b19c52cebb1b
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-08-18 14:55:56 +0300

    Make changes for asp.net core.

--- aspnet-core/src/AbpCompanyName.AbpProjectName.Web.Host/Startup/Program.cs
+++ aspnet-core/src/AbpCompanyName.AbpProjectName.Web.Host/Startup/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.IO;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace AbpCompanyName.AbpProjectName.Web.Host.Startup
@@ -7,14 +7,14 @@ namespace AbpCompanyName.AbpProjectName.Web.Host.Startup
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
-
-            host.Run();
         }
     }
 }
