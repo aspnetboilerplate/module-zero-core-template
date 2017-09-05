db8872f838eeb867b639078b2d3a83ba55811382
Aaron Chong <aaronjhchong@hotmail.com>
2017-09-06 02:00:14 +0800

    Fix localization key in RoleAppService

--- aspnet-core/src/AbpCompanyName.AbpProjectName.Application/Roles/RoleAppService.cs
+++ aspnet-core/src/AbpCompanyName.AbpProjectName.Application/Roles/RoleAppService.cs
@@ -75,7 +75,7 @@ namespace AbpCompanyName.AbpProjectName.Roles
             var role = await _roleManager.FindByIdAsync(input.Id.ToString());
             if (role.IsStatic)
             {
-                throw new UserFriendlyException("CannotDeleteAStaticRole");
+                throw new UserFriendlyException(L("CanNotDeleteStaticRole"));
             }
 
             var users = await _userManager.GetUsersInRoleAsync(role.NormalizedName);
