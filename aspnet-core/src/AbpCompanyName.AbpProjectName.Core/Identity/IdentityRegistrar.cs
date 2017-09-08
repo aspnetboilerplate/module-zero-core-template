1761f2c5daf7ca6d01ca6c5bdbbf4faaf5f6c6c0
İsmail ÇAGDAŞ <ismailcagdas@gmail.com>
2017-09-08 10:21:11 +0300

    changed dentityRegistrar.Regiter return type

--- aspnet-core/src/AbpCompanyName.AbpProjectName.Core/Identity/IdentityRegistrar.cs
+++ aspnet-core/src/AbpCompanyName.AbpProjectName.Core/Identity/IdentityRegistrar.cs
@@ -10,11 +10,11 @@ namespace AbpCompanyName.AbpProjectName.Identity
 {
     public static class IdentityRegistrar
     {
-        public static void Register(IServiceCollection services)
+        public static IdentityBuilder Register(IServiceCollection services)
         {
             services.AddLogging();
 
-            services.AddAbpIdentity<Tenant, User, Role>()
+            return services.AddAbpIdentity<Tenant, User, Role>()
                 .AddAbpTenantManager<TenantManager>()
                 .AddAbpUserManager<UserManager>()
                 .AddAbpRoleManager<RoleManager>()
