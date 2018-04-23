2aa46831f5b3c28ed8a175cb8812c62f50aec1b4
İsmail ÇAGDAŞ <ismailcagdas@gmail.com>
2018-04-23 14:48:22 +0300

    fixed #224: use abp.ico instead of td_icon.ico

--- vue/build/webpack.prod.config.js
+++ vue/build/webpack.prod.config.js
@@ -57,7 +57,7 @@ module.exports = merge(webpackBaseConfig, {
         // }),
         new CopyWebpackPlugin([
             {
-                from: 'td_icon.ico'
+                from: 'abp.ico'
             },
             {
                 from: 'src/styles/fonts',
@@ -91,7 +91,7 @@ module.exports = merge(webpackBaseConfig, {
         }),
         new HtmlWebpackPlugin({
             title: 'iView admin v' + package.version,
-            favicon: './td_icon.ico',
+            favicon: './abp.ico',
             filename: '../index.html',
             template: '!!ejs-loader!./src/template/index.ejs',
             inject: false
