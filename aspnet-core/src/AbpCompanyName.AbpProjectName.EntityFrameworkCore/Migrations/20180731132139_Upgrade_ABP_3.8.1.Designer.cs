8db25bbd90ee96963e8e918199d23cbead4bd3e0
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2018-07-31 16:23:22 +0300

    Modify UserToken.MaxLoginProviderLength to 128. Fixed #314

--- aspnet-core/src/AbpCompanyName.AbpProjectName.EntityFrameworkCore/Migrations/20180731132139_Upgrade_ABP_3.8.1.Designer.cs
+++ aspnet-core/src/AbpCompanyName.AbpProjectName.EntityFrameworkCore/Migrations/20180731132139_Upgrade_ABP_3.8.1.Designer.cs
@@ -4,14 +4,16 @@ using AbpCompanyName.AbpProjectName.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AbpCompanyName.AbpProjectName.Migrations
 {
     [DbContext(typeof(AbpProjectNameDbContext))]
-    partial class AbpProjectNameDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180731132139_Upgrade_ABP_3.8.1")]
+    partial class Upgrade_ABP_381
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
