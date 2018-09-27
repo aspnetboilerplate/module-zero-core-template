86c19715ca3e1d473a73aad0b1968a1216194347
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2018-09-27 09:28:35 +0300

    Resolved #335 - Upgrade ABP to v3.8.3

---
+++ aspnet-core/src/AbpCompanyName.AbpProjectName.EntityFrameworkCore/Migrations/20180927062608_Upgrade_ABP_3.8.3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AbpCompanyName.AbpProjectName.Migrations
+{
+    public partial class Upgrade_ABP_383 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExpireDate",
+                table: "AbpUserTokens",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpireDate",
+                table: "AbpUserTokens");
+        }
+    }
+}
