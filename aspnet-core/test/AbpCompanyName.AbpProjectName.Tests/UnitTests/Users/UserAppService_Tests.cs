65bc6c98fd0b514ba1138edc6ca96b78fbb8480f
Lee Richardson <github@leerichardson.com>
2018-10-13 10:09:52 -0400

    Refactor, add more GetRole tests

--- aspnet-core/test/AbpCompanyName.AbpProjectName.Tests/UnitTests/Users/UserAppService_Tests.cs
+++ aspnet-core/test/AbpCompanyName.AbpProjectName.Tests/UnitTests/Users/UserAppService_Tests.cs
@@ -14,9 +14,7 @@ namespace AbpCompanyName.AbpProjectName.Tests.UnitTests.Users
         {
             // Arrange
             var role = new Role(29, "John Nash");
-            var roleRepository = role.AsRepository<Role>();
-            var userAppService = new UserAppService(null, null, null, roleRepository, null);
-            userAppService.ObjectMapper = ObjectMapper;
+            var userAppService = GetUserAppService(role);
 
             // Act
             var listResultDto = await userAppService.GetRoles();
@@ -24,6 +22,29 @@ namespace AbpCompanyName.AbpProjectName.Tests.UnitTests.Users
             // Assert
             var item = listResultDto.Items.ShouldHaveSingleItem();
             item.DisplayName.ShouldBe("John Nash");
+        }
+
+        [Fact]
+        public async Task GivenNoRoleExists_WhenGetRoles_ThenNothingIsReturned()
+        {
+            // Arrange
+            var userAppService = GetUserAppService(null);
+
+            // Act
+            var listResultDto = await userAppService.GetRoles();
+
+            // Assert
+            listResultDto.Items.ShouldBeEmpty();
+        }
+
+        private UserAppService GetUserAppService(Role role)
+        {
+            var roleRepository = role.AsRepository<Role>();
+            var userAppService = new UserAppService(null, null, null, roleRepository, null)
+            {
+                ObjectMapper = ObjectMapper
+            };
+            return userAppService;
         }
     }
 }
