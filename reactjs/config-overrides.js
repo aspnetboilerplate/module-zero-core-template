82e969212d7c4bc8a854703e0768cf5554444210
Rashiduddin Yoldash <ryoldash@hotmail.com>
2018-10-27 19:02:41 +0300

    test

---
+++ reactjs/config-overrides.js
@@ -0,0 +1,34 @@
+const tsImportPluginFactory = require('ts-import-plugin')
+const {
+    getLoader
+} = require("react-app-rewired");
+const rewireLess = require('react-app-rewire-less');
+
+module.exports = function override(config, env) {
+    const tsLoader = getLoader(
+        config.module.rules,
+        rule =>
+        rule.loader &&
+        typeof rule.loader === 'string' &&
+        rule.loader.includes('ts-loader')
+    );
+
+    tsLoader.options = {
+        getCustomTransformers: () => ({
+            before: [tsImportPluginFactory({
+                libraryDirectory: 'es',
+                libraryName: 'antd',
+                style: 'css',
+            })]
+        })
+    };
+
+    config = rewireLess.withLoaderOptions({
+        javascriptEnabled: true,
+        modifyVars: {
+            "@primary-color": "#1DA57A"
+        },
+    })(config, env);
+
+    return config;
+}
