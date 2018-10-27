1df94cb7b35a90c2bce7ce76d173f7783fc77395
Rashiduddin Yoldash <ryoldash@hotmail.com>
2018-10-27 18:09:46 +0300

    Ant Design added

--- reactjs/src/components/AbpComponentBase.tsx
+++ reactjs/src/components/AbpComponentBase.tsx
@@ -9,29 +9,53 @@ import { IMessageService } from "src/services/message/messageService";
 import { IAbpMultiTenancyService } from "src/services/multiTenancy/abpMultiTenancyService";
 import { inject } from "inversify";
 import { IAppSessionService } from "src/services/session/appSessionService";
+import SERVICE_IDENTIFIER from "src/config/identifiers";
 
 export abstract class AbpComponentBase extends React.Component {
   localizationSourceName = AppConsts.localization.defaultLocalizationSourceName;
 
-  localization: ILocalizationService;
-  permission: IPermissionCheckerService;
-  feature: IFeatureCheckerService;
-  notify: INotifyService;
-  setting: ISettingService;
-  message: IMessageService;
-  multiTenancy: IAbpMultiTenancyService;
-  appSession: IAppSessionService;
+  _localization: ILocalizationService;
+  _permission: IPermissionCheckerService;
+  _feature: IFeatureCheckerService;
+  _notify: INotifyService;
+  _setting: ISettingService;
+  _message: IMessageService;
+  _multiTenancy: IAbpMultiTenancyService;
+  _appSession: IAppSessionService;
   // elementRef: ElementRef;
 
   /**
    *
    */
-  constructor() {
-    super();
+  constructor(
+    @inject(SERVICE_IDENTIFIER.ILocalizationService)
+    localization: ILocalizationService,
+    @inject(SERVICE_IDENTIFIER.IPermissionCheckerService)
+    permission: IPermissionCheckerService,
+    @inject(SERVICE_IDENTIFIER.IFeatureCheckerService)
+    feature: IFeatureCheckerService,
+    @inject(SERVICE_IDENTIFIER.INotifyService) notify: INotifyService,
+    @inject(SERVICE_IDENTIFIER.ISettingService) setting: ISettingService,
+    @inject(SERVICE_IDENTIFIER.IMessageService) message: IMessageService,
+    @inject(SERVICE_IDENTIFIER.IAbpMultiTenancyService)
+    multenancy: IAbpMultiTenancyService,
+    @inject(SERVICE_IDENTIFIER.IAppSessionService)
+    appSession: IAppSessionService
+  ) {
+    super(localization);
+
+    this._localization = localization;
+    this._permission = permission;
+    this._feature = feature;
+    this._notify = notify;
+    this._setting = setting;
+    this._message = message;
+    this._multiTenancy = multenancy;
+    this._appSession = appSession;
   }
 
   l(key: string, ...args: any[]): string {
-    let localizedText = this.localization.localize(
+    let localizedText = this._localization.localize(
       key,
       this.localizationSourceName
     );
@@ -49,6 +73,6 @@ export abstract class AbpComponentBase extends React.Component {
   }
 
   isGranted(permissionName: string): boolean {
-    return this.permission.isGranted(permissionName);
+    return this._permission.isGranted(permissionName);
   }
 }
