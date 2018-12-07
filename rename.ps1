06b7ae52a9db42977f4fa358f6cd16b21e41784f
personball <personball@163.com>
2018-12-07 13:07:13 +0800

    translate rename.ps1

--- rename.ps1
+++ rename.ps1
@@ -1,26 +1,24 @@
-﻿# 设置输出格式
+﻿# set output encoding
 $OutputEncoding = [Text.UTF8Encoding]::UTF8
 
-## 公共参数
-
-# 替换前的公司名称
+# company name placeholder 
 $oldCompanyName="AbpCompanyName"
-# 替换后的公司名称
+# your company name
 $newCompanyName="YourCompanyName"
 
-# 替换前的项目名称
+# project name placeholder
 $oldProjectName="AbpProjectName"
-# 替换后的项目名称
+# your project name
 $newProjectName="YourProjectName"
 
-# 文件类型名称
+# file type
 $fileType="FileInfo"
 
-# 目录类型名称
+# directory type
 $dirType="DirectoryInfo"
 
-#复制一份
-Write-Host '开始复制文件夹'
+# copy 
+Write-Host 'Start copy folders...'
 $newRoot=$newCompanyName+"."+$newProjectName
 mkdir $newRoot
 Copy-Item -Recurse .\aspnet-core\ .\$newRoot\
@@ -29,7 +27,7 @@ Copy-Item .gitignore .\$newRoot\
 Copy-Item LICENSE .\$newRoot\
 Copy-Item README.md .\$newRoot\
 
-# sln所在目录
+# folders to deal with
 $slnFolder = (Get-Item -Path "./$newRoot/aspnet-core/" -Verbose).FullName
 $vueFolder = (Get-Item -Path "./$newRoot/vue/" -Verbose).FullName
 
@@ -41,24 +39,24 @@ function Rename {
 		$NewCompanyName,
 		$NewProjectName
 	)
-	# 需要修改文件内容的文件后缀名
+	# file extensions to deal with
 	$include=@("*.cs","*.cshtml","*.asax","*.ps1","*.ts","*.csproj","*.sln","*.xaml","*.json","*.js","*.xml","*.config","Dockerfile")
 
 	$elapsed = [System.Diagnostics.Stopwatch]::StartNew()
 
-	Write-Host "[$TargetFolder]开始重命名文件夹"
-	# 重命名文件夹
+	Write-Host "[$TargetFolder]Start rename folder..."
+	# rename folder
 	Ls $TargetFolder -Recurse | Where { $_.GetType().Name -eq $dirType -and ($_.Name.Contains($PlaceHolderCompanyName) -or $_.Name.Contains($PlaceHolderProjectName)) } | ForEach-Object{
 		Write-Host 'directory ' $_.FullName
 		$newDirectoryName=$_.Name.Replace($PlaceHolderCompanyName,$NewCompanyName).Replace($PlaceHolderProjectName,$NewProjectName)
 		Rename-Item $_.FullName $newDirectoryName
 	}
-	Write-Host "[$TargetFolder]结束重命名文件夹"
+	Write-Host "[$TargetFolder]End rename folder."
 	Write-Host '-------------------------------------------------------------'
 
 
-	# 替换文件中的内容和文件名
-	Write-Host "[$TargetFolder]开始替换文件中的内容和文件名"
+	# replace file content and rename file name
+	Write-Host "[$TargetFolder]Start replace file content and rename file name..."
 	Ls $TargetFolder -Include $include -Recurse | Where { $_.GetType().Name -eq $fileType} | ForEach-Object{
 		$fileText = Get-Content $_ -Raw -Encoding UTF8
 		if($fileText.Length -gt 0 -and ($fileText.contains($PlaceHolderCompanyName) -or $fileText.contains($PlaceHolderProjectName))){
@@ -71,11 +69,11 @@ function Rename {
 			Write-Host 'file(change name) ' $_.FullName
 		}
 	}
-	Write-Host "[$TargetFolder]结束替换文件中的内容和文件名"
+	Write-Host "[$TargetFolder]End replace file content and rename file name."
 	Write-Host '-------------------------------------------------------------'
 
 	$elapsed.stop()
-	write-host "[$TargetFolder]共花费时间: $($elapsed.Elapsed.ToString())"
+	write-host "[$TargetFolder]Total Time Cost: $($elapsed.Elapsed.ToString())"
 }
 
 Rename -TargetFolder $slnFolder -PlaceHolderCompanyName $oldCompanyName -PlaceHolderProjectName $oldProjectName -NewCompanyName $newCompanyName -NewProjectName $newProjectName
