cbd86268d1167d50ba1d685e3640a30bb06fa703
Ismail Yilmaz <iyilm4z@gmail.com>
2018-12-24 22:10:33 +0300

    Migrate account login to @angular/material components

--- angular/src/account/login/login.component.ts
+++ angular/src/account/login/login.component.ts
@@ -1,52 +1,39 @@
-﻿import { Component, Injector, ElementRef, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
-import { AppComponentBase } from '@shared/app-component-base';
-import { LoginService } from './login.service';
-import { accountModuleAnimation } from '@shared/animations/routerTransition';
+﻿import { Component, Injector } from '@angular/core';
 import { AbpSessionService } from '@abp/session/abp-session.service';
+import { AppComponentBase } from '@shared/app-component-base';
+import { accountModuleAnimation } from '@shared/animations/routerTransition';
+import { LoginService } from './login.service';
 
 @Component({
-    templateUrl: './login.component.html',
-    styleUrls: [
-        './login.component.less'
-    ],
-    animations: [accountModuleAnimation()]
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.less'],
+  animations: [accountModuleAnimation()]
 })
 export class LoginComponent extends AppComponentBase {
+  submitting: boolean = false;
 
-    @ViewChild('cardBody') cardBody: ElementRef;
+  constructor(
+    injector: Injector,
+    private loginService: LoginService,
+    private _sessionService: AbpSessionService
+  ) {
+    super(injector);
+  }
 
-    submitting: boolean = false;
+  get multiTenancySideIsTeanant(): boolean {
+    return this._sessionService.tenantId > 0;
+  }
 
-    constructor(
-        injector: Injector,
-        public loginService: LoginService,
-        private _router: Router,
-        private _sessionService: AbpSessionService
-    ) {
-        super(injector);
+  get isSelfRegistrationAllowed(): boolean {
+    if (!this._sessionService.tenantId) {
+      return false;
     }
 
-    ngAfterViewInit(): void {
-        $(this.cardBody.nativeElement).find('input:first').focus();
-    }
+    return true;
+  }
 
-    get multiTenancySideIsTeanant(): boolean {
-        return this._sessionService.tenantId > 0;
-    }
-
-    get isSelfRegistrationAllowed(): boolean {
-        if (!this._sessionService.tenantId) {
-            return false;
-        }
-
-        return true;
-    }
-
-    login(): void {
-        this.submitting = true;
-        this.loginService.authenticate(
-            () => this.submitting = false
-        );
-    }
+  login(): void {
+    this.submitting = true;
+    this.loginService.authenticate(() => (this.submitting = false));
+  }
 }
