037e98368606f82453e92d748fd09a1af48f4996
Rashiduddin Yoldash <ryoldash@hotmail.com>
2019-01-07 22:37:36 +0300

    Typescript added

--- react-native/App.tsx
+++ react-native/App.tsx
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, { Component } from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
 
