ea2b593a981943ccb0a9cbf7d65505f85d07fd2b
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2019-01-11 14:50:57 +0300

    Resolved conflicts

--- angular/src/app/roles/roles.component.ts
+++ angular/src/app/roles/roles.component.ts
@@ -3,18 +3,18 @@ import { MatDialog } from '@angular/material';
 import { finalize } from 'rxjs/operators';
 import { appModuleAnimation } from '@shared/animations/routerTransition';
 import {
-  PagedListingComponentBase,
-  PagedRequestDto
+    PagedListingComponentBase,
+    PagedRequestDto
 } from '@shared/paged-listing-component-base';
 import {
-  RoleServiceProxy,
-  RoleDto,
-  PagedResultDtoOfRoleDto
+    RoleServiceProxy,
+    RoleDto,
+    PagedResultDtoOfRoleDto
 } from '@shared/service-proxies/service-proxies';
 import { CreateRoleDialogComponent } from './create-role/create-role-dialog.component';
 import { EditRoleDialogComponent } from './edit-role/edit-role-dialog.component';
 
-class PagedRolesRequestDto extends PagedRequestDto{
+class PagedRolesRequestDto extends PagedRequestDto {
     keyword: string;
 }
 
@@ -23,75 +23,75 @@ class PagedRolesRequestDto extends PagedRequestDto{
     animations: [appModuleAnimation()]
 })
 export class RolesComponent extends PagedListingComponentBase<RoleDto> {
-  roles: RoleDto[] = [];
+    roles: RoleDto[] = [];
 
-  constructor(
-    injector: Injector,
-    private _rolesService: RoleServiceProxy,
-    private _dialog: MatDialog
-  ) {
-    super(injector);
-  }
-
-  list(
-    request: PagedRequestDto,
-    pageNumber: number,
-    finishedCallback: Function
-  ): void {
-    this._rolesService
-      .getAll(request.skipCount, request.maxResultCount)
-      .pipe(
-        finalize(() => {
-          finishedCallback();
-        })
-      )
-      .subscribe((result: PagedResultDtoOfRoleDto) => {
-        this.roles = result.items;
-        this.showPaging(result, pageNumber);
-      });
-  }
-
-  delete(role: RoleDto): void {
-    abp.message.confirm(
-      this.l('RoleDeleteWarningMessage', role.displayName),
-      (result: boolean) => {
-        if (result) {
-          this._rolesService
-            .delete(role.id)
-            .pipe(
-              finalize(() => {
-                abp.notify.success(this.l('SuccessfullyDeleted'));
-                this.refresh();
-              })
-            )
-            .subscribe(() => { });
-        }
-      }
-    );
-  }
-
-  createRole(): void {
-    this.showCreateOrEditRoleDialog();
-  }
-
-  editRole(role: RoleDto): void {
-    this.showCreateOrEditRoleDialog(role.id);
-  }
-
-  showCreateOrEditRoleDialog(id?: number): void {
-    let createOrEditRoleDialog;
-    if (id === undefined || id <= 0) {
-      createOrEditRoleDialog = this._dialog.open(CreateRoleDialogComponent);
-    } else {
-      createOrEditRoleDialog = this._dialog.open(EditRoleDialogComponent, {
-        data: id
-      });
+    constructor(
+        injector: Injector,
+        private _rolesService: RoleServiceProxy,
+        private _dialog: MatDialog
+    ) {
+        super(injector);
     }
-    
-    createOrEditRoleDialog.afterClosed().subscribe(result => {
-      if (result) {
-        this.refresh();
-      }
-    });
-  }
+
+    list(
+        request: PagedRolesRequestDto,
+        pageNumber: number,
+        finishedCallback: Function
+    ): void {
+        this._rolesService
+            .getAll(request.keyword, request.skipCount, request.maxResultCount)
+            .pipe(
+                finalize(() => {
+                    finishedCallback();
+                })
+            )
+            .subscribe((result: PagedResultDtoOfRoleDto) => {
+                this.roles = result.items;
+                this.showPaging(result, pageNumber);
+            });
+    }
+
+    delete(role: RoleDto): void {
+        abp.message.confirm(
+            this.l('RoleDeleteWarningMessage', role.displayName),
+            (result: boolean) => {
+                if (result) {
+                    this._rolesService
+                        .delete(role.id)
+                        .pipe(
+                            finalize(() => {
+                                abp.notify.success(this.l('SuccessfullyDeleted'));
+                                this.refresh();
+                            })
+                        )
+                        .subscribe(() => { });
+                }
+            }
+        );
+    }
+
+    createRole(): void {
+        this.showCreateOrEditRoleDialog();
+    }
+
+    editRole(role: RoleDto): void {
+        this.showCreateOrEditRoleDialog(role.id);
+    }
+
+    showCreateOrEditRoleDialog(id?: number): void {
+        let createOrEditRoleDialog;
+        if (id === undefined || id <= 0) {
+            createOrEditRoleDialog = this._dialog.open(CreateRoleDialogComponent);
+        } else {
+            createOrEditRoleDialog = this._dialog.open(EditRoleDialogComponent, {
+                data: id
+            });
+        }
+
+        createOrEditRoleDialog.afterClosed().subscribe(result => {
+            if (result) {
+                this.refresh();
+            }
+        });
+    }
 }
--- angular/src/app/tenants/tenants.component.ts
+++ angular/src/app/tenants/tenants.component.ts
@@ -3,96 +3,96 @@ import { MatDialog } from '@angular/material';
 import { finalize } from 'rxjs/operators';
 import { appModuleAnimation } from '@shared/animations/routerTransition';
 import {
-  PagedListingComponentBase,
-  PagedRequestDto
+    PagedListingComponentBase,
+    PagedRequestDto
 } from 'shared/paged-listing-component-base';
 import {
-  TenantServiceProxy,
-  TenantDto,
-  PagedResultDtoOfTenantDto
+    TenantServiceProxy,
+    TenantDto,
+    PagedResultDtoOfTenantDto
 } from '@shared/service-proxies/service-proxies';
 import { CreateTenantDialogComponent } from './create-tenant/create-tenant-dialog.component';
 import { EditTenantDialogComponent } from './edit-tenant/edit-tenant-dialog.component';
 
-class PagedTenantsRequestDto extends PagedRequestDto{
+class PagedTenantsRequestDto extends PagedRequestDto {
     keyword: string;
     isActive: boolean | null;
 }
 
 @Component({
-  templateUrl: './tenants.component.html',
-  animations: [appModuleAnimation()]
+    templateUrl: './tenants.component.html',
+    animations: [appModuleAnimation()]
 })
 export class TenantsComponent extends PagedListingComponentBase<TenantDto> {
-  tenants: TenantDto[] = [];
+    tenants: TenantDto[] = [];
 
-  constructor(
-    injector: Injector,
-    private _tenantService: TenantServiceProxy,
-    private _dialog: MatDialog
-  ) {
-    super(injector);
-  }
-
-  list(
-    request: PagedRequestDto,
-    pageNumber: number,
-    finishedCallback: Function
-  ): void {
-    this._tenantService
-      .getAll(request.skipCount, request.maxResultCount)
-      .pipe(
-        finalize(() => {
-          finishedCallback();
-        })
-      )
-      .subscribe((result: PagedResultDtoOfTenantDto) => {
-        this.tenants = result.items;
-        this.showPaging(result, pageNumber);
-      });
-  }
-
-  delete(tenant: TenantDto): void {
-    abp.message.confirm(
-      this.l('TenantDeleteWarningMessage', tenant.name),
-      (result: boolean) => {
-        if (result) {
-          this._tenantService
-            .delete(tenant.id)
-            .pipe(
-              finalize(() => {
-                abp.notify.success(this.l('SuccessfullyDeleted'));
-                this.refresh();
-              })
-            )
-            .subscribe(() => {});
-        }
-      }
-    );
-  }
-
-  createTenant(): void {
-    this.showCreateOrEditTenantDialog();
-  }
-
-  editTenant(tenant: TenantDto): void {
-    this.showCreateOrEditTenantDialog(tenant.id);
-  }
-
-  showCreateOrEditTenantDialog(id?: number): void {
-    let createOrEditTenantDialog;
-    if (id === undefined || id <= 0) {
-      createOrEditTenantDialog = this._dialog.open(CreateTenantDialogComponent);
-    } else {
-      createOrEditTenantDialog = this._dialog.open(EditTenantDialogComponent, {
-        data: id
-      });
+    constructor(
+        injector: Injector,
+        private _tenantService: TenantServiceProxy,
+        private _dialog: MatDialog
+    ) {
+        super(injector);
     }
 
-    createOrEditTenantDialog.afterClosed().subscribe(result => {
-      if (result) {
-        this.refresh();
-      }
-    });
-  }
+    list(
+        request: PagedTenantsRequestDto,
+        pageNumber: number,
+        finishedCallback: Function
+    ): void {
+        this._tenantService
+            .getAll(request.keyword, request.isActive, request.skipCount, request.maxResultCount)
+            .pipe(
+                finalize(() => {
+                    finishedCallback();
+                })
+            )
+            .subscribe((result: PagedResultDtoOfTenantDto) => {
+                this.tenants = result.items;
+                this.showPaging(result, pageNumber);
+            });
+    }
+
+    delete(tenant: TenantDto): void {
+        abp.message.confirm(
+            this.l('TenantDeleteWarningMessage', tenant.name),
+            (result: boolean) => {
+                if (result) {
+                    this._tenantService
+                        .delete(tenant.id)
+                        .pipe(
+                            finalize(() => {
+                                abp.notify.success(this.l('SuccessfullyDeleted'));
+                                this.refresh();
+                            })
+                        )
+                        .subscribe(() => { });
+                }
+            }
+        );
+    }
+
+    createTenant(): void {
+        this.showCreateOrEditTenantDialog();
+    }
+
+    editTenant(tenant: TenantDto): void {
+        this.showCreateOrEditTenantDialog(tenant.id);
+    }
+
+    showCreateOrEditTenantDialog(id?: number): void {
+        let createOrEditTenantDialog;
+        if (id === undefined || id <= 0) {
+            createOrEditTenantDialog = this._dialog.open(CreateTenantDialogComponent);
+        } else {
+            createOrEditTenantDialog = this._dialog.open(EditTenantDialogComponent, {
+                data: id
+            });
+        }
+
+        createOrEditTenantDialog.afterClosed().subscribe(result => {
+            if (result) {
+                this.refresh();
+            }
+        });
+    }
 }
