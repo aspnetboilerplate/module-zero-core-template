ca145e1a0bf7d89a761ef62e2f5d801085fe2617
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2019-02-08 08:18:19 +0300

    Fixes breaking changes

--- aspnet-core/src/AbpCompanyName.AbpProjectName.Application/Users/Dto/UserMapProfile.cs
+++ aspnet-core/src/AbpCompanyName.AbpProjectName.Application/Users/Dto/UserMapProfile.cs
@@ -10,8 +10,7 @@ namespace AbpCompanyName.AbpProjectName.Users.Dto
             CreateMap<UserDto, User>();
             CreateMap<UserDto, User>()
                 .ForMember(x => x.Roles, opt => opt.Ignore())
-                .ForMember(x => x.CreationTime, opt => opt.Ignore())
-                .ForMember(x => x.LastLoginTime, opt => opt.Ignore());
+                .ForMember(x => x.CreationTime, opt => opt.Ignore());
 
             CreateMap<CreateUserDto, User>();
             CreateMap<CreateUserDto, User>().ForMember(x => x.Roles, opt => opt.Ignore());
