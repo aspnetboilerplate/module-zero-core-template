f8acbbc12d95534d3d8f8365e375db37099bb23f
İsmail ÇAGDAŞ <ismailcagdas@gmail.com>
2019-02-26 16:02:10 +0300

    changed app creation for tests

--- reactjs/src/App.test.tsx
+++ reactjs/src/App.test.tsx
@@ -1,9 +1,23 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Provider } from 'mobx-react';
+import { BrowserRouter } from 'react-router-dom';
+import initializeStores from './stores/storeInitializer';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  //ReactDOM.render(<App />, div);
+
+  const stores = initializeStores();
+
+  ReactDOM.render(
+    <Provider {...stores}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
