ff80a5a57ed700be57814437a6bc08f3e6324b8c
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2019-03-13 15:13:59 +0300

    Upgrade ABP to v4.4. Resolved: #421

--- aspnet-core/src/AbpCompanyName.AbpProjectName.Core/Authorization/Roles/RoleManager.cs
+++ aspnet-core/src/AbpCompanyName.AbpProjectName.Core/Authorization/Roles/RoleManager.cs
@@ -3,7 +3,9 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Logging;
 using Abp.Authorization;
 using Abp.Authorization.Roles;
+using Abp.Domain.Repositories;
 using Abp.Domain.Uow;
+using Abp.Organizations;
 using Abp.Runtime.Caching;
 using Abp.Zero.Configuration;
 using AbpCompanyName.AbpProjectName.Authorization.Users;
@@ -13,24 +15,28 @@ namespace AbpCompanyName.AbpProjectName.Authorization.Roles
     public class RoleManager : AbpRoleManager<Role, User>
     {
         public RoleManager(
-            RoleStore store, 
-            IEnumerable<IRoleValidator<Role>> roleValidators, 
-            ILookupNormalizer keyNormalizer, 
-            IdentityErrorDescriber errors, 
+            RoleStore store,
+            IEnumerable<IRoleValidator<Role>> roleValidators,
+            ILookupNormalizer keyNormalizer,
+            IdentityErrorDescriber errors,
             ILogger<AbpRoleManager<Role, User>> logger,
-            IPermissionManager permissionManager, 
-            ICacheManager cacheManager, 
+            IPermissionManager permissionManager,
+            ICacheManager cacheManager,
             IUnitOfWorkManager unitOfWorkManager,
-            IRoleManagementConfig roleManagementConfig)
+            IRoleManagementConfig roleManagementConfig,
+            IRepository<OrganizationUnit, long> organizationUnitRepository,
+            IRepository<OrganizationUnitRole, long> organizationUnitRoleRepository)
             : base(
                   store,
-                  roleValidators, 
-                  keyNormalizer, 
-                  errors, logger, 
+                  roleValidators,
+                  keyNormalizer,
+                  errors, logger,
                   permissionManager,
-                  cacheManager, 
+                  cacheManager,
                   unitOfWorkManager,
-                  roleManagementConfig)
+                  roleManagementConfig,
+                organizationUnitRepository,
+                organizationUnitRoleRepository)
         {
         }
     }
