039aa761966d47c6ea158652068f6c2732a5f915
Vitor Durante <vitcoppidu@gmail.com>
2019-04-03 15:51:12 -0300

    add headers to sidebar navigation

--- angular/src/app/layout/sidebar-nav.component.ts
+++ angular/src/app/layout/sidebar-nav.component.ts
@@ -13,10 +13,12 @@ export class SideBarNavComponent extends AppComponentBase {
         new MenuItem(this.l('HomePage'), '', 'home', '/app/home'),
 
         new MenuItem(this.l('Tenants'), 'Pages.Tenants', 'business', '/app/tenants'),
+
         new MenuItem(this.l('Users'), 'Pages.Users', 'people', '/app/users'),
         new MenuItem(this.l('Roles'), 'Pages.Roles', 'local_offer', '/app/roles'),
         new MenuItem(this.l('About'), '', 'info', '/app/about'),
 
+        new MenuItem(this.l('Static'), '', '', ''),
         new MenuItem(this.l('MultiLevelMenu'), '', 'menu', '', [
             new MenuItem('ASP.NET Boilerplate', '', '', '', [
                 new MenuItem('Home', '', '', 'https://aspnetboilerplate.com/?ref=abptmpl'),
