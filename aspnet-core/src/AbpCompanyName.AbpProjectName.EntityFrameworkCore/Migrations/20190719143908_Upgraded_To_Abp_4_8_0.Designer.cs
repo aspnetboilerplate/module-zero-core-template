84fefee05bac577fa6651515b9eae3bdeb83f83e
Musa Demir <musa.demir@volosoft.com>
2019-07-19 17:41:58 +0300

    abp packages are updated.

--- aspnet-core/src/AbpCompanyName.AbpProjectName.EntityFrameworkCore/Migrations/20190719143908_Upgraded_To_Abp_4_8_0.Designer.cs
+++ aspnet-core/src/AbpCompanyName.AbpProjectName.EntityFrameworkCore/Migrations/20190719143908_Upgraded_To_Abp_4_8_0.Designer.cs
@@ -4,14 +4,16 @@ using AbpCompanyName.AbpProjectName.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AbpCompanyName.AbpProjectName.Migrations
 {
     [DbContext(typeof(AbpProjectNameDbContext))]
-    partial class AbpProjectNameDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190719143908_Upgraded_To_Abp_4_8_0")]
+    partial class Upgraded_To_Abp_4_8_0
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
