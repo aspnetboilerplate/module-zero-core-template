2dd1d7a05c70fef95badc6cfae6946271cae94c1
maliming <malimings@gmail.com>
2019-08-02 16:29:57 +0800

    Eliminate warning messages for the project.

--- aspnet-core/src/AbpCompanyName.AbpProjectName.Application/AbpProjectNameApplicationModule.cs
+++ aspnet-core/src/AbpCompanyName.AbpProjectName.Application/AbpProjectNameApplicationModule.cs
@@ -23,7 +23,7 @@ namespace AbpCompanyName.AbpProjectName
 
             Configuration.Modules.AbpAutoMapper().Configurators.Add(
                 // Scan the assembly for classes which inherit from AutoMapper.Profile
-                cfg => cfg.AddProfiles(thisAssembly)
+                cfg => cfg.AddMaps(thisAssembly)
             );
         }
     }
