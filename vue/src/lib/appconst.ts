df9846b42632923740fe0b9ee7b8b80777b25c4c
ccccccmd <admin@rootu.cn>
2019-12-26 13:34:05 +0800

    remove trailing slash in appconst'remoteServiceBaseUrl in Vue progect

--- vue/src/lib/appconst.ts
+++ vue/src/lib/appconst.ts
@@ -1,15 +1,15 @@
 import url from './url'
-const AppConsts= {
-    userManagement:{
+const AppConsts = {
+    userManagement: {
         defaultAdminUserName: 'admin'
     },
-    localization:{
+    localization: {
         defaultLocalizationSourceName: 'AbpProjectName'
     },
-    authorization:{
+    authorization: {
         encrptedAuthTokenName: 'enc_auth_token'
     },
     appBaseUrl: "http://localhost:8080",
-    remoteServiceBaseUrl:url
+    remoteServiceBaseUrl: url.endsWith('/') ? url.slice(0, -1) : url
 }
 export default AppConsts
