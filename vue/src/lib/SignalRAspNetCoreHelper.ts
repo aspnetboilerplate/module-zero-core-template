b9342372eb89101c8ee127a9b21c98f6e49230d8
ccccccmd <admin@rootu.cn>
2019-12-26 13:17:29 +0800

    remove trailing slash in SignalRAspNetCoreHelper'remoteServiceBaseUrl in Vue progect

--- vue/src/lib/SignalRAspNetCoreHelper.ts
+++ vue/src/lib/SignalRAspNetCoreHelper.ts
@@ -3,13 +3,14 @@ import Util from './util'
 class SignalRAspNetCoreHelper {
     initSignalR() {
         var encryptedAuthToken = Util.abp.utils.getCookieValue(AppConsts.authorization.encrptedAuthTokenName);
+        let remoteServerUrl = AppConsts.remoteServiceBaseUrl;
         Util.abp.signalr = {
             autoConnect: true,
             connect: undefined,
             hubs: undefined,
             qs: AppConsts.authorization.encrptedAuthTokenName + "=" + encodeURIComponent(encryptedAuthToken),
-            remoteServiceBaseUrl: AppConsts.remoteServiceBaseUrl,
-            url: 'signalr'
+            remoteServiceBaseUrl: remoteServerUrl.endsWith('/') ? remoteServerUrl.slice(0, -1) : remoteServerUrl,
+            url: '/signalr'
         };
 
         Util.loadScript(AppConsts.appBaseUrl + '/dist/abp.signalr-client.js');
