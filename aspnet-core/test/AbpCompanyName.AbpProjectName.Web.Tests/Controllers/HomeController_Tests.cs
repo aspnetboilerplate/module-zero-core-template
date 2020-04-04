f4b3d6c79e9292349dd401338a90dc6bab973405
maliming <malimings@gmail.com>
2020-04-04 21:59:00 +0800

    Add Web.Tests project.

---
+++ aspnet-core/test/AbpCompanyName.AbpProjectName.Web.Tests/Controllers/HomeController_Tests.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using AbpCompanyName.AbpProjectName.Models.TokenAuth;
+using AbpCompanyName.AbpProjectName.Web.Controllers;
+using Shouldly;
+using Xunit;
+
+namespace AbpCompanyName.AbpProjectName.Web.Tests.Controllers
+{
+    public class HomeController_Tests: AbpProjectNameWebTestBase
+    {
+        [Fact]
+        public async Task Index_Test()
+        {
+            await AuthenticateAsync(null, new AuthenticateModel
+            {
+                UserNameOrEmailAddress = "admin",
+                Password = "123qwe"
+            });
+
+            //Act
+            var response = await GetResponseAsStringAsync(
+                GetUrl<HomeController>(nameof(HomeController.Index))
+            );
+
+            //Assert
+            response.ShouldNotBeNullOrEmpty();
+        }
+    }
+}
---
+++ aspnet-core/test/AbpCompanyName.AbpProjectName.Web.Tests/Startup.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Abp.AspNetCore;
+using Abp.AspNetCore.TestBase;
+using Abp.Dependency;
+using AbpCompanyName.AbpProjectName.Authentication.JwtBearer;
+using AbpCompanyName.AbpProjectName.Configuration;
+using AbpCompanyName.AbpProjectName.EntityFrameworkCore;
+using AbpCompanyName.AbpProjectName.Identity;
+using AbpCompanyName.AbpProjectName.Web.Resources;
+using AbpCompanyName.AbpProjectName.Web.Startup;
+using Castle.MicroKernel.Registration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace AbpCompanyName.AbpProjectName.Web.Tests
+{
+    public class Startup
+    {
+        private readonly IConfigurationRoot _appConfiguration;
+
+        public Startup(IWebHostEnvironment env)
+        {
+            _appConfiguration = env.GetAppConfiguration();
+        }
+
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddEntityFrameworkInMemoryDatabase();
+
+            services.AddMvc();
+            
+            IdentityRegistrar.Register(services);
+            AuthConfigurer.Configure(services, _appConfiguration);
+            
+            services.AddScoped<IWebResourceManager, WebResourceManager>();
+
+            //Configure Abp and Dependency Injection
+            return services.AddAbp<AbpProjectNameWebTestModule>(options =>
+            {
+                options.SetupTest();
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+            UseInMemoryDb(app.ApplicationServices);
+
+            app.UseAbp(); //Initializes ABP framework.
+
+            app.UseExceptionHandler("/Error");
+
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseJwtTokenMiddleware();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        private void UseInMemoryDb(IServiceProvider serviceProvider)
+        {
+            var builder = new DbContextOptionsBuilder<AbpProjectNameDbContext>();
+            builder.UseInMemoryDatabase(Guid.NewGuid().ToString()).UseInternalServiceProvider(serviceProvider);
+            var options = builder.Options;
+
+            var iocManager = serviceProvider.GetRequiredService<IIocManager>();
+            iocManager.IocContainer
+                .Register(
+                    Component.For<DbContextOptions<AbpProjectNameDbContext>>()
+                        .UsingFactoryMethod((kernel, context) =>
+                        {
+                            var qq = 1;
+                            return options;
+                        })
+                        .LifestyleSingleton()
+                );
+        }
+    }
+}
