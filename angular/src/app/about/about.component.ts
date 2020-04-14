bd8b27d42341c4aa12a7f68ce6313e4cdbe0efcd
iyilm4z <iyilm4z@gmail.com>
2020-04-14 19:39:15 +0300

    app's about comp done

--- angular/src/app/about/about.component.ts
+++ angular/src/app/about/about.component.ts
@@ -1,16 +1,14 @@
-import { Component, Injector, AfterViewInit } from '@angular/core';
+import { Component, Injector, ChangeDetectionStrategy } from '@angular/core';
 import { AppComponentBase } from '@shared/app-component-base';
 import { appModuleAnimation } from '@shared/animations/routerTransition';
 
 @Component({
-    templateUrl: './about.component.html',
-    animations: [appModuleAnimation()]
+  templateUrl: './about.component.html',
+  animations: [appModuleAnimation()],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AboutComponent extends AppComponentBase {
-
-    constructor(
-        injector: Injector
-    ) {
-        super(injector);
-    }
+  constructor(injector: Injector) {
+    super(injector);
+  }
 }
