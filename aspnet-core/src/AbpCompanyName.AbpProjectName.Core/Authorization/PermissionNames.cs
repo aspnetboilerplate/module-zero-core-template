fa2e0a9c224deb299673033f0691410934b45fd0
Farid Bekran <faridbekran15@gmail.com>
2020-11-15 10:11:36 +0330

    Added activation api

--- aspnet-core/src/AbpCompanyName.AbpProjectName.Core/Authorization/PermissionNames.cs
+++ aspnet-core/src/AbpCompanyName.AbpProjectName.Core/Authorization/PermissionNames.cs
@@ -5,6 +5,7 @@
         public const string Pages_Tenants = "Pages.Tenants";
 
         public const string Pages_Users = "Pages.Users";
+        public const string Pages_Users_Activation = "Pages.Users.Activation";
 
         public const string Pages_Roles = "Pages.Roles";
     }
