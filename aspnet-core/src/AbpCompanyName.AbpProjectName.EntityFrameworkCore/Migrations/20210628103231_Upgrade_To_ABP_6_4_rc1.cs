fd96f30b1270b67e1b60cee67a3ba35e378a39a7
Musa Demir <demirmusa96@gmail.com>
2021-06-28 13:32:47 +0300

    Add migration

---
+++ aspnet-core/src/AbpCompanyName.AbpProjectName.EntityFrameworkCore/Migrations/20210628103231_Upgrade_To_ABP_6_4_rc1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AbpCompanyName.AbpProjectName.Migrations
+{
+    public partial class Upgrade_To_ABP_6_4_rc1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpUserLogins_ProviderKey_TenantId",
+                table: "AbpUserLogins",
+                columns: new[] { "ProviderKey", "TenantId" },
+                unique: true,
+                filter: "[TenantId] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AbpUserLogins_ProviderKey_TenantId",
+                table: "AbpUserLogins");
+        }
+    }
+}
