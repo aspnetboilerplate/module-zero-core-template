7f53787aab76665b7495d9f1a6e28ce095407acc
İsmail ÇAĞDAŞ <ismailcagdas@gmail.com>
2021-08-31 11:40:28 +0300

    resolves https://github.com/aspnetboilerplate/aspnetboilerplate/issues/6238: Use a static value for DefaultPassPhrase in debug mode

---
+++ aspnet-core/src/AbpCompanyName.AbpProjectName.Core/Debugging/DebugHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace AbpCompanyName.AbpProjectName.Debugging
+{
+    public static class DebugHelper
+    {
+        public static bool IsDebug
+        {
+            get
+            {
+#pragma warning disable
+#if DEBUG
+                return true;
+#endif
+                return false;
+#pragma warning restore
+            }
+        }
+    }
+}
