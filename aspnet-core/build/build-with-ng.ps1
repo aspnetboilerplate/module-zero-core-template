6dbfe2fa0ca8e394e94bb18206ff1cfe186f94d3
İsmail ÇAĞDAŞ <ismailcagdas@gmail.com>
2023-02-13 15:34:31 +0300

    change location of docker files of Host project

--- aspnet-core/build/build-with-ng.ps1
+++ aspnet-core/build/build-with-ng.ps1
@@ -3,11 +3,12 @@ echo ""
 echo ""
 
 $ABP_HOST="abp/host"
+$ABP_HOST_DOCKERFILE_PATH="src/AbpCompanyName.AbpProjectName.Web.Host/Dockerfile"
 $ABP_NG="abp/ng"
 
 cd ..
 echo " Building docker image $ABP_HOST..."
-docker build -t $ABP_HOST .
+docker build -t $ABP_HOST -f $ABP_HOST_DOCKERFILE_PATH . 
 echo " Done. -- Building docker image $ABP_HOST..."
 echo ""
 echo ""
---
+++ aspnet-core/src/AbpCompanyName.AbpProjectName.Web.Mvc/Dockerfile
@@ -0,0 +1,6 @@
+FROM mcr.microsoft.com/dotnet/core/aspnet:2.2-stretch-slim
+
+WORKDIR /app
+COPY . .
+
+ENTRYPOINT ["dotnet", "AbpCompanyName.AbpProjectName.Web.Host.dll"]
