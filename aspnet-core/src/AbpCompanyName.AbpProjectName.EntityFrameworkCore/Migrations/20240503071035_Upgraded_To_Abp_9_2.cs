e5895d712bb6c020ef3bd9cfb635d0c0be76d860
Oğuzhan Ağır <90545225+oguzhanagir@users.noreply.github.com>
2024-05-03 10:12:08 +0300

    added abp 9.2 migration

---
+++ aspnet-core/src/AbpCompanyName.AbpProjectName.EntityFrameworkCore/Migrations/20240503071035_Upgraded_To_Abp_9_2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AbpCompanyName.AbpProjectName.Migrations
+{
+    /// <inheritdoc />
+    public partial class Upgraded_To_Abp_9_2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FailReason",
+                table: "AbpUserLoginAttempts",
+                type: "nvarchar(1024)",
+                maxLength: 1024,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FailReason",
+                table: "AbpUserLoginAttempts");
+        }
+    }
+}
