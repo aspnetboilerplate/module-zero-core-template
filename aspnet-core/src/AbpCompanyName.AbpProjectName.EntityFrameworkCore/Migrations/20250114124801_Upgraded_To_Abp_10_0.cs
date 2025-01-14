b3130a6e62b06d0c1fc215083f353c2357630c59
Oğuzhan Ağır <90545225+oguzhanagir@users.noreply.github.com>
2025-01-14 15:54:01 +0300

    Updated Migrations for Abp 10.0

---
+++ aspnet-core/src/AbpCompanyName.AbpProjectName.EntityFrameworkCore/Migrations/20250114124801_Upgraded_To_Abp_10_0.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AbpCompanyName.AbpProjectName.Migrations
+{
+    /// <inheritdoc />
+    public partial class Upgraded_To_Abp_10_0 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Parameters",
+                table: "AbpAuditLogs",
+                type: "nvarchar(2048)",
+                maxLength: 2048,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1024)",
+                oldMaxLength: 1024,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Parameters",
+                table: "AbpAuditLogs",
+                type: "nvarchar(1024)",
+                maxLength: 1024,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2048)",
+                oldMaxLength: 2048,
+                oldNullable: true);
+        }
+    }
+}
